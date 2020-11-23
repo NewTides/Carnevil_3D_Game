8d4cb152273c5510645904aa6700212e701fbce4
NewTides <jlevy.75583@farnborough.ac.uk>
2020-11-23 10:16:23 +0000

    Ghost activity mechanic added HUD to track in game, console logs for other stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
@@ -7,7 +10,21 @@ public class PlayerController : MonoBehaviour
 	private CharacterController _characterController;
 	private Vector3 _characterVelocity;
 	private bool _isSprinting;
+	/*
+	private bool _area1Active;
+	private bool _area2Active;
+	private bool _area3Active;
+	private bool _area4Active;
+	*/
+	private float activityTimer = 0;
+	private int area1Activity = 0;
+	private int area2Activity = 0;
+	private int area3Activity = 0;
+	private int area4Activity = 0;
 	
+	[SerializeField, Tooltip("Description of this object")]
+	private string description = "";
+
 	[SerializeField][Tooltip("The force applied to the character when jumping")]
 	private float JumpForce = 10f;
 
@@ -45,6 +62,9 @@ public class PlayerController : MonoBehaviour
 		_characterController = GetComponent<CharacterController>();
 	}
 
+	
+	
+	
 	// Update is called once per frame
 	private void Update()
 	{
@@ -96,8 +116,102 @@ public class PlayerController : MonoBehaviour
 		
 		// Apply calculated movement
 		_characterController.Move(_characterVelocity * Time.deltaTime);
+		
+		
 	}
 
+	/**/
+	private void OnTriggerEnter(Collider other)
+	{
+		print("Player has entered the area of" + other.name);
+		description = "Ghost Activity has temporarily subsided";
+		DisplayDetailUI();
+	}
+	/**/
+
+	void OnTriggerStay(Collider other)
+	{
+		activityTimer += Time.deltaTime;
+		if (activityTimer >= 1f && other.name == "Area1")
+		{
+			area1Activity += 2;
+			area2Activity -= 1;
+			area3Activity -= 1;
+			area4Activity -= 1;
+			activityTimer = 0;
+		}else if (activityTimer >= 1f && other.name == "Area2")
+		{
+			area2Activity += 2;
+			area1Activity -= 1;
+			area3Activity -= 1;
+			area4Activity -= 1;
+			activityTimer = 0;
+		}else if (activityTimer >= 1f && other.name == "Area3")
+		{
+			area3Activity += 2;
+			area1Activity -= 1;
+			area2Activity -= 1;
+			area4Activity -= 1;
+			activityTimer = 0;
+		}else if (activityTimer >= 1f && other.name == "Area4")
+		{
+			area4Activity += 2;
+			area1Activity -= 1;
+			area2Activity -= 1;
+			area3Activity -= 1;
+			activityTimer = 0;
+		}
+		
+		if (area1Activity < 0)
+		{
+			area1Activity = 0;
+		} else if (area2Activity < 0)
+		{
+			area2Activity = 0;
+		} else if (area3Activity < 0)
+		{
+			area3Activity = 0;
+		}else if (area4Activity < 0)
+		{
+			area4Activity = 0;
+		}
+
+		print("Area1: " + area1Activity);
+		print("Area2: " + area2Activity);
+		print("Area3: " + area3Activity);
+		print("Area4: " + area4Activity);
+		
+		if (area1Activity >= 120 || area2Activity >= 120 || area3Activity >= 120 || area4Activity >= 120)
+		{
+			print("Ghost activity has reached or exceeded critical spookums, you are now dead.");
+			description = "Ghost activity has reached or exceeded critical spookums, you are now dead.";
+		}
+
+		if ((area1Activity == 30 && other.name == "Area1") || (area2Activity == 30 && other.name == "Area2") || (area3Activity == 30 && other.name == "Area3") || (area4Activity == 30 && other.name == "Area4"))
+		{
+			description = "Ghost activity in the local area ('" + other.name + "') is currently low.";
+			print(description);
+		}
+		if ((area1Activity == 60 && other.name == "Area1") || (area2Activity == 60 && other.name == "Area2") || (area3Activity == 60 && other.name == "Area3") || (area4Activity == 60 && other.name == "Area4"))
+		{
+			description = "Ghost activity in the local area ('" + other.name + "') is currently moderate but rising.";
+			print(description);
+		}
+		if ((area1Activity == 90 && other.name == "Area1") || (area2Activity == 90 && other.name == "Area2") || (area3Activity == 90 && other.name == "Area3") || (area4Activity == 90 && other.name == "Area4"))
+		{
+			description = "Ghost activity in the local area ('" + other.name + "') is currently high.";
+			print(description);
+		}
+		DisplayDetailUI();
+	}
+	
+	public virtual void DisplayDetailUI()
+	{
+		Debug.Log($"Description:\n {description}");
+		HUD.Instance.SetOutputText($"{description}");
+	}
+	
+	
 	private void HandleAirMovement() 
 	{
 		// cache the input axes
