b3eaf1caca123971a61646786f6a1bf9c05bcaa7
NewTides <jlevy.75583@farnborough.ac.uk>
2021-01-12 22:39:42 +0000

    Cusor lock & hide implemented - hold Esc to free the mouse. Thresholds for activity updates and death due to ghost activity doubled. Camera sensitivity fixed & movement tweaked. Jump temporarily re-enabled to navigate terrain. New models given mesh colliders. Re-implemented temporary HUD, now in the bottom right of the screen.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,7 @@ public class PlayerController : MonoBehaviour
 	private const float Gravity = 9.8f;
 	private CharacterController _characterController;
 	private Vector3 _characterVelocity;
-	private bool _isSprinting;
-	/*
-	private bool _area1Active;
-	private bool _area2Active;
-	private bool _area3Active;
-	private bool _area4Active;
-	*/
+	//private bool _isSprinting;
 	private float activityTimer = 0;
 	private int area1Activity = 0;
 	private int area2Activity = 0;
@@ -26,38 +20,40 @@ public class PlayerController : MonoBehaviour
 	private string description = "";
 
 	[SerializeField][Tooltip("The force applied to the character when jumping")]
-	private float JumpForce = 10f;
-
+	private float JumpForce = 3f;
+	
 	[SerializeField][Tooltip("The camera for the first person controller")]
 	public Camera _Camera;
 
 	[SerializeField]
 	[Tooltip("The fall speed modifier, increases gravity")]
-	private float GravityMultiplier = 2f;
+	private float GravityMultiplier = 1f;
 	
 	[SerializeField][Tooltip("Walking speed of the character")]
 	private Vector3 WalkingSpeed = new Vector3(2f,0f,4f); 	
 	
-	[SerializeField][Tooltip("Sprinting speed of the character")]
-	private Vector3 SprintSpeed = new Vector3(2f,0f,9f); 
+	//[SerializeField][Tooltip("Sprinting speed of the character")]
+	//private Vector3 SprintSpeed = new Vector3(2f,0f,9f); 
 	
 	[SerializeField][Tooltip("Walking speed of the character")]
 	private Vector3 AirStrafeSpeed = new Vector3(0.01f,0f,0.01f); 	
 	
 	[SerializeField][Tooltip("Minimum angle the camera can look up/down")]
-	private float MinCameraVerticalAngle = -70;
+	private float MinCameraVerticalAngle = -90;
 	[SerializeField][Tooltip("Maximum angle the camera can look up/down")]
-	private float MaxCameraVerticalAngle = 55;
+	private float MaxCameraVerticalAngle = 90;
 	
 	[SerializeField][Tooltip("The speed at which the camera moves")]
-	private Vector2 CameraSpeed = new Vector2(.5f,0.1f);
+	private Vector2 CameraSpeed = new Vector2(.1f,0.1f);
 
-	[SerializeField][Tooltip("Does the sprint button need to be held? false = toggle")]
-	private bool HoldToSprint;
+	//[SerializeField][Tooltip("Does the sprint button need to be held? false = toggle")]
+	//private bool HoldToSprint;
 	
 	// Start is called before the first frame update
 	private void Start()
 	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 		// cache the character controller component
 		_characterController = GetComponent<CharacterController>();
 	}
@@ -68,7 +64,7 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	private void Update()
 	{
-
+		// Hold Esc to stop the camera moving and unlock the cursor
 		HandleCameraMovement();
 		
 		// Was i grounded last frame?
@@ -76,7 +72,7 @@ public class PlayerController : MonoBehaviour
 
 		//TODO: Crouch
 
-		if (HoldToSprint)
+		/* if (HoldToSprint)
 		{
 			//sprint hold
 			_isSprinting = Input.GetButton("Sprint");
@@ -88,7 +84,7 @@ public class PlayerController : MonoBehaviour
 			{
 				_isSprinting = !_isSprinting;
 			}
-		}
+		} */
 
 		if (_characterController.isGrounded)
 		{
@@ -101,7 +97,7 @@ public class PlayerController : MonoBehaviour
 				_characterVelocity.y = 0;
 
 				// add jump velocity
-				//_characterVelocity += Vector3.up * JumpForce;
+				_characterVelocity += Vector3.up * JumpForce;
 			}
 
 		}		
@@ -117,14 +113,13 @@ public class PlayerController : MonoBehaviour
 		// Apply calculated movement
 		_characterController.Move(_characterVelocity * Time.deltaTime);
 		
-		
 	}
 
 	/**/
-	private void OnTriggerEnter(Collider other)
+	private void OnTriggerExit(Collider other)
 	{
 		print("Player has entered the area of" + other.name);
-		description = "Ghost Activity has temporarily subsided";
+		description = "Ghost Activity is negligible... for now";
 		DisplayDetailUI();
 	}
 	/**/
@@ -181,23 +176,23 @@ public class PlayerController : MonoBehaviour
 		print("Area3: " + area3Activity);
 		print("Area4: " + area4Activity);
 		
-		if (area1Activity >= 120 || area2Activity >= 120 || area3Activity >= 120 || area4Activity >= 120)
+		if (area1Activity >= 240 || area2Activity >= 240 || area3Activity >= 240 || area4Activity >= 240)
 		{
 			print("Ghost activity has reached or exceeded critical spookums, you are now dead.");
 			description = "Ghost activity has reached or exceeded critical spookums, you are now dead.";
 		}
 
-		if ((area1Activity == 30 && other.name == "Area1") || (area2Activity == 30 && other.name == "Area2") || (area3Activity == 30 && other.name == "Area3") || (area4Activity == 30 && other.name == "Area4"))
+		if ((area1Activity == 60 && other.name == "Area1") || (area2Activity == 60 && other.name == "Area2") || (area3Activity == 60 && other.name == "Area3") || (area4Activity == 60 && other.name == "Area4"))
 		{
 			description = "Ghost activity in the local area ('" + other.name + "') is currently low.";
 			print(description);
 		}
-		if ((area1Activity == 60 && other.name == "Area1") || (area2Activity == 60 && other.name == "Area2") || (area3Activity == 60 && other.name == "Area3") || (area4Activity == 60 && other.name == "Area4"))
+		if ((area1Activity == 120 && other.name == "Area1") || (area2Activity == 120 && other.name == "Area2") || (area3Activity == 120 && other.name == "Area3") || (area4Activity == 120 && other.name == "Area4"))
 		{
 			description = "Ghost activity in the local area ('" + other.name + "') is currently moderate but rising.";
 			print(description);
 		}
-		if ((area1Activity == 90 && other.name == "Area1") || (area2Activity == 90 && other.name == "Area2") || (area3Activity == 90 && other.name == "Area3") || (area4Activity == 90 && other.name == "Area4"))
+		if ((area1Activity == 180 && other.name == "Area1") || (area2Activity == 180 && other.name == "Area2") || (area3Activity == 180 && other.name == "Area3") || (area4Activity == 180 && other.name == "Area4"))
 		{
 			description = "Ghost activity in the local area ('" + other.name + "') is currently high.";
 			print(description);
@@ -235,8 +230,11 @@ public class PlayerController : MonoBehaviour
 		
 		// convert to a vector representing movement directions
 		Vector3 inputAsMovement = new Vector3(inputAxes.x, 0f, inputAxes.y);
+		
+		// multiply movement direction by speed  
+		inputAsMovement.Scale(WalkingSpeed);
 
-		if (!_isSprinting)
+		/* if (!_isSprinting)
 		{
 			// multiply movement direction by speed  
 			inputAsMovement.Scale(WalkingSpeed);
@@ -245,7 +243,7 @@ public class PlayerController : MonoBehaviour
 		{
 			// multiply movement direction by speed  
 			inputAsMovement.Scale(SprintSpeed);
-		}
+		} */
 
 		// transform the movement vector into world space relative to the player
 		Vector3 worldSpaceMovement = transform.TransformVector(inputAsMovement);
@@ -253,28 +251,45 @@ public class PlayerController : MonoBehaviour
 	}
 
 	private float _cameraVerticalAngle;
-	
+
 	private void HandleCameraMovement()
 	{
-		// cache the camera input axes
-		Vector2 inputAxes = new Vector2(Input.GetAxisRaw("Horizontal_Camera"),Input.GetAxisRaw("Vertical_Camera"));
-		//Debug.Log($"Hor:{Input.GetAxisRaw("Horizontal_Camera")} Ver:{Input.GetAxisRaw("Vertical_Camera")}");
+		//Unlock Cursor
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		else
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+			
+			// cache the camera input axes
+			Vector2 inputAxes = new Vector2(Input.GetAxisRaw("Horizontal_Camera"), Input.GetAxisRaw("Vertical_Camera"));
+			//Debug.Log($"Hor:{Input.GetAxisRaw("Horizontal_Camera")} Ver:{Input.GetAxisRaw("Vertical_Camera")}");
 
-		// scale the input by the camera speed
-		inputAxes.Scale(CameraSpeed);
-		
-		// horizontal rotation - rotate the character
-		transform.Rotate(Vector3.up, inputAxes.x,Space.Self);
-		
-		// vertical rotation - rotate the camera
-		// add new input to the accumulated angle
-		_cameraVerticalAngle += inputAxes.y;
-		
-		// clamp the vertical rotation to be between min and max angles
-		_cameraVerticalAngle = Mathf.Clamp(_cameraVerticalAngle, MinCameraVerticalAngle, MaxCameraVerticalAngle);
-		
-		// apply to the camera
-		_Camera.transform.localEulerAngles = new Vector3(_cameraVerticalAngle,0f,0f);
+			// scale the input by the camera speed
+			inputAxes.Scale(CameraSpeed);
+
+			// horizontal rotation - rotate the character
+			transform.Rotate(Vector3.up, inputAxes.x, Space.Self);
+
+			// vertical rotation - rotate the camera
+			// add new input to the accumulated angle
+			_cameraVerticalAngle += inputAxes.y;
+
+			// clamp the vertical rotation to be between min and max angles
+			_cameraVerticalAngle = Mathf.Clamp(_cameraVerticalAngle, MinCameraVerticalAngle, MaxCameraVerticalAngle);
+
+			// apply to the camera
+			_Camera.transform.localEulerAngles = new Vector3(_cameraVerticalAngle, 0f, 0f);
+		}
+	}
+
+	private void Menu()
+	{
+		throw new NotImplementedException();
 	}
 
 	private void OnDrawGizmos()
